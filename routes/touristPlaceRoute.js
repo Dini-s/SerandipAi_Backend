6e2e870127b1e374f974fef7606e8128b1509d34
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2026-02-26 14:25:09 +0530

    implement route for tourist place

---
+++ routes/touristPlaceRoute.js
@@ -0,0 +1,13 @@
+import express from "express";
+import verifyToken from "../middleware/authMiddleware";
+import { upload } from "../config/multer";
+import { getAllTouristPlaces, getNearbyPlaces, getTouristPlaceById } from "../controllers/touristPlaceController";
+
+const router = express.Router();
+
+router.post("/AddNew", verifyToken, upload.array("images", 5), addNewPlace);
+router.get("/", getAllTouristPlaces);
+router.get("/nearby", getNearbyPlaces);
+router.get("/:id", getTouristPlaceById);
+
+export default router;
